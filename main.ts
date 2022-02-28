4587e79a28b8b3c086afdb070492c751cfbeac9b
Haydeniscool <100564395+Haydeniscool@users.noreply.github.com>
2022-02-28 11:23:58 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let Im_confused = 0
+basic.forever(function () {
+    if (true) {
+        Im_confused = 7
+    } else {
+        music.setVolume(127)
+        music.playMelody("- G F G F G F F ", 400)
+    }
+})
